5b54f793f952c5943bc564158e32c19d91349b2b
konstantinaxt <axtkonstantin@hotmail.de>
2019-04-21 17:25:55 +0200

    remove shipit and add gulp-rsync for deployment

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require("gulp");
+const argv = require("minimist")(process.argv);
+const rsync = require("gulp-rsync");
+
+gulp.task("deploy", () => {
+
+  const rsyncPaths = ["build"];
+  const rsyncConf = {
+    progress: true,
+    incremental: true,
+    relative: true,
+    emptyDirectories: true,
+    hostname: "ssh.strato.de",
+    username: "derriegl.de",
+    archive: true,
+    silent: false,
+    compress: true,
+    recursive: true,
+    clean: true,
+    exclude: [],
+  };
+
+  if (argv.staging) {
+    rsyncConf.destination = "staging";
+  } else {
+    rsyncConf.destination = "production";
+  }
+
+  return gulp.src(rsyncPaths)
+             .pipe(rsync(rsyncConf));
+
+});
