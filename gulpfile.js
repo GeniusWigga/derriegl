425eccfeeeca85f674106eea575269261c77781f
konstantinaxt <axtkonstantin@hotmail.de>
2019-06-18 14:35:07 +0200

    refactor build scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require("gulp");
 const argv = require("minimist")(process.argv);
 const rsync = require("gulp-rsync");
+const exec = require("child_process").exec;
+
+const watch = gulp.watch;
+const series = gulp.series;
 
 gulp.task("deploy", () => {
-
-  const rsyncPaths = ["dist"];
   const rsyncConf = {
     progress: true,
     incremental: true,
@@ -26,7 +28,19 @@ gulp.task("deploy", () => {
     rsyncConf.destination = "production";
   }
 
-  return gulp.src(rsyncPaths)
+  return gulp.src(["dist"])
              .pipe(rsync(rsyncConf));
 
 });
+
+function build(cb) {
+  return exec("npm run start", function(err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+}
+
+exports.default = function() {
+  watch("src", series(build));
+};
--- static.config.js
+++ static.config.js
@@ -1,36 +1,50 @@
-import path from 'path'
-import axios from 'axios'
+import path from "path";
+import axios from "axios";
 
 export default {
   getRoutes: async () => {
     const { data: posts } = await axios.get(
-      'https://jsonplaceholder.typicode.com/posts'
-    )
+      "https://jsonplaceholder.typicode.com/posts",
+    );
 
     return [
       {
-        path: '/blog',
+        path: "/blog",
         getData: () => ({
           posts,
         }),
         children: posts.map(post => ({
           path: `/post/${post.id}`,
-          template: 'src/containers/Post',
+          template: "src/containers/Post",
           getData: () => ({
             post,
           }),
         })),
       },
-    ]
+    ];
+  },
+  getSiteData: () => {
+    return {
+      menu: {
+        home: {
+          de: "Der Riegel",
+          en: "Der Riegel",
+        },
+        ingredients: {
+          "de": "Unsere Zutaten",
+          "en": "Our ingredients",
+        },
+      },
+    };
   },
   plugins: [
     [
-      require.resolve('react-static-plugin-source-filesystem'),
+      require.resolve("react-static-plugin-source-filesystem"),
       {
-        location: path.resolve('./src/pages'),
+        location: path.resolve("./src/pages"),
       },
     ],
-    require.resolve('react-static-plugin-reach-router'),
-    require.resolve('react-static-plugin-sitemap'),
+    require.resolve("react-static-plugin-reach-router"),
+    require.resolve("react-static-plugin-sitemap"),
   ],
-}
+};
