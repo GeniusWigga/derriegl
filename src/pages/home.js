089cc1713a6d85c4ae87451b75f57189488455b4
konstantinaxt <axtkonstantin@hotmail.de>
2019-06-20 18:40:20 +0200

    add badge to hero, start implement slider

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactMarkdown from "react-markdown/with-html";
+import Swiper from "react-id-swiper";
 
 import { useRouteData } from "react-static";
 
 import Layout from "../containers/Layout";
+import Badge from "../components/Icons/Badge";
 
 import "./home.scss";
 
@@ -16,7 +18,29 @@ export default () => {
     <Layout {...routeData} className="home">
       <div className="home__hero">
         <img className="home__hero-img" src="/img/hero.jpg" alt="home hero" />
+        <div className="home__badge-wrapper">
+          <Badge className="home__badge" />
+        </div>
         <ReactMarkdown escapeHtml={false} className="home__hero-headline" source={translations.headline} />
+      </div>
+      <div className="home__products">
+
+        <h2 className="home__products-headline">{translations.products.headline}</h2>
+
+        <Swiper>
+          <div>
+            <h3 className="home__products-sub-headline">{translations.products.natural.headline}</h3>
+
+            <div className="home__products-intro">
+              <img className="home__products-image" src="/img/products/natural.png" alt="natural image" />
+            </div>
+          </div>
+          <div>Slide 2</div>
+          <div>Slide 3</div>
+          <div>Slide 4</div>
+          <div>Slide 5</div>
+        </Swiper>
+
       </div>
     </Layout>
   );
