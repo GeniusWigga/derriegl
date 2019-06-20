0700dcab9e5fe7f24d6c7074cabfc16ea55a5543
konstantinaxt <axtkonstantin@hotmail.de>
2019-06-20 14:08:28 +0200

    add styling to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,8 +9,14 @@ export default ({ translations }) => {
 
   return (
     <nav className="header">
+      <div className="header__logo-wrapper">
+        <img className="header__logo" src="/img/logo.png" alt="Der Riegel Logo" />
+      </div>
       <Link className="header__item" to="/">{header.home}</Link>
-      <Link className="header__item" to="/about">{header.ingredients}</Link>
+      <Link className="header__item" to="/#ingredients">{header.ingredients}</Link>
+      <Link className="header__item" to="/#about">{header.aboutus}</Link>
+      <Link className="header__item" to="/#buy">{header.buy}</Link>
+      <Link className="header__item" to="/#contact">{header.contact}</Link>
     </nav>
   );
 }
